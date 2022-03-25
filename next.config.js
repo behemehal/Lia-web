50b92c49a5847c7135e42cede6947013db820278
Ahmetcan Aksu <ahmetcanco@gmail.com>
2022-03-25 11:37:58 +0300

    Update icon assets

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
-const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
-module.exports = { basePath: "/Lia-web", assetPrefix: "/Lia-web/" };
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/Lia-web/_next/static/images/',
+  }],
+  {
+    basePath: '/Lia-web',
+    assetPrefix: '/Lia-web/',
+    env,
+  },
+]);
