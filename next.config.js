ff21eefb8119194914f7e63eabdd2a3da62868ad
Ahmetcan Aksu <ahmetcanco@gmail.com>
2022-03-25 11:35:04 +0300

    update assets path

--- next.config.js
+++ next.config.js
@@ -1,3 +1,3 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
-module.exports = { basePath: "/Lia-Web", assetPrefix: "/Lia-Web/" };
+module.exports = { basePath: "/Lia-web", assetPrefix: "/Lia-web/" };
