67f45b5093bd80dd033b51f028fd53c13b9b07a9
Ahmetcan Aksu <ahmetcanco@gmail.com>
2022-03-25 11:28:27 +0300

    Fix nex.config issue

--- next.config.js
+++ next.config.js
@@ -1,19 +1,3 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-module.exports = withPlugins([
-  [optimizedImages, {
-    mozjpeg: {
-      quality: 80,
-    },
-    pngquant: {
-      speed: 3,
-      strip: true,
-      verbose: true,
-    },
-    imagesPublicPath: '/Lia-Web/_next/static/images/',
-  }],
-  {
-    basePath: '/Lia-Web',
-    assetPrefix: '/Lia-Web/',
-  },
-]);
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+module.exports = { basePath: "/Lia-Web", assetPrefix: "/Lia-Web/" };
