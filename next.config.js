2972454c159d17f24163e85c54ca92bcdcb4f92b
Ahmetcan Aksu <ahmetcanco@gmail.com>
2022-03-25 11:21:36 +0300

    Update env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-  distDir: "build",
-};
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/Lia-Web/_next/static/images/',
+  }],
+  {
+    basePath: '/Lia-Web',
+    assetPrefix: '/Lia-Web/',
+  },
+]);
